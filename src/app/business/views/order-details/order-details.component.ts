0be7aa4b97b0916141534890b6c14825c0dda7c1
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2023-03-22 18:37:11 +0530

    fix order statuses

--- src/app/business/views/order-details/order-details.component.ts
+++ src/app/business/views/order-details/order-details.component.ts
@@ -28,10 +28,6 @@ export class OrderDetailsComponent {
     {
       label: 'Order Picked Up',
       value: 'picked_up'
-    },
-    {
-      label: 'Delivered',
-      value: 'delivered'
     }
   ]
 
@@ -52,7 +48,13 @@ export class OrderDetailsComponent {
         this.order = response.data;
         this.order.status_text_color = this.order.order_status === 'cancelled' ? 'text-danger' : 'text-success';
         this.order.order_status_text = this.order.order_status.replace(/_/g, ' ');
-        this.orderStatus = this.order.order_status === 'delivered' ? 'picked_up' : this.order.order_status;
+        this.orderStatus = this.order.order_status;
+        if (this.order.order_status === 'delivered') {
+          this.orderStatuses.push({
+            label: 'Delivered',
+            value: 'delivered'
+          })
+        }
         // order.order_status_text to title case
         this.order.order_status_text = this.order.order_status_text.replace(/\w\S*/g, (txt: any) => {
           return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
@@ -68,6 +70,7 @@ export class OrderDetailsComponent {
   }
 
   updateOrderStatus(order_status: string) {
+    console.log(order_status)
     this.ordersService.updateOrderStatus(this.orderID, order_status).subscribe({
       next: (response: any) => {
         console.log(response);
